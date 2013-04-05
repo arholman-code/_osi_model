349012a38e34444ad5d4d8fa55ba2a14d42b7995
arholman-code <arholman.code@gmail.com>
2013-04-05 12:22:23 -0300

    Update SineWave.java

--- SineWave.java
+++ SineWave.java
@@ -19,8 +19,8 @@ public class SineWave extends JPanel{
        phase = p;
     }
 
-    public void advance(int phaseIncrement) {
-       phase -= phaseIncrement;
+    public void move(int phaseShift) {
+       phase -= phaseShift;
        if(phase >= 360) {
             phase %= 360;
         }
@@ -33,13 +33,6 @@ public class SineWave extends JPanel{
        return new SineWave(wavelength, amplitude, phase);
     }
 
-    public int getY(int x) {
-       double degreesPerPixel = 360.0 / wavelength;
-       double degrees = (-phase+180) + x*degreesPerPixel;
-       return (int)(amplitude*Math.sin(degrees*Math.PI/180.0));
-    }
-
-
     public void draw(Graphics g, int x, int y, int length) 
     {   for (int i=1; i<=length; i++)
         {  Point p1 = new Point(x+i, y+getY(i));
@@ -47,16 +40,6 @@ public class SineWave extends JPanel{
            g.drawLine(p1.x, p1.y, p2.x, p2.y);
         }
     }
-
-    public int getPhase(float length) 
-    {   return (int)(360*(length/wavelength) + phase);
-    }
-
-    public  float getWaveNum(int x) {
-       return (float)(x/wavelength);
-    }
-
-  
 }
 
 
